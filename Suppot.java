27504f0f7a7609825e1a79b9cd733f753b21796f
AfonsoDGBatista <113676683+AfonsoDGBatista@users.noreply.github.com>
2022-10-10 21:15:46 +0100

    Add files via upload

---
+++ Suppot.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Suppot {
+
+    /**
+     * Default constructor
+     */
+    public Suppot() {
+    }
+
+    /**
+     * 
+     */
+    public ERole role;
+
+    /**
+     * 
+     */
+    public float hours;
+
+}
